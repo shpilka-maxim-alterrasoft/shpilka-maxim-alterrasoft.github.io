34c6a71363cf5b227857c100f5b4d9cad4767098
shpilka-maxim-alterrasoft <50993763+shpilka-maxim-alterrasoft@users.noreply.github.com>
2019-06-03 14:05:22 +0300

    Add files via upload

---
+++ precache-manifest.3df6366d092228d6bc93fc38882f07b7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac157cc1c1178fb17bcf17e811bd9d0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4663128a245a3b3b8e2b",
+    "url": "/static/css/2.cdaf15d1.chunk.css"
+  },
+  {
+    "revision": "ddeed5d0482649a78caa",
+    "url": "/static/css/main.a595bdba.chunk.css"
+  },
+  {
+    "revision": "4663128a245a3b3b8e2b",
+    "url": "/static/js/2.cc957102.chunk.js"
+  },
+  {
+    "revision": "ddeed5d0482649a78caa",
+    "url": "/static/js/main.8045757b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c213105a963dd9d681e47c0f89890863",
+    "url": "/static/media/background.c213105a.svg"
+  },
+  {
+    "revision": "81c88265d87a5d0bc5d8b7c79e01b66b",
+    "url": "/static/media/logo.81c88265.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
